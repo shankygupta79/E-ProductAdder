3bd4f5bd60942f1db81e6450c2f2c34a1dcbbe90
Shubham Gupta <shankygupta79@gmail.com>
2020-02-22 23:36:01 +0530

    DB CHANGES

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 //dbname','username','pass'
-const db = new Sequelize('cp1jpd6gJR', 'cp1jpd6gJR', 'Ropg5v6S06', {
-    host:'remotemysql.com',
+const db = new Sequelize('database79', 'database79', 'hello3878', {
+    host:'db4free.net',
     dialect: 'mysql',
     port:3306,
   operatorsAliases: false,
@@ -41,7 +41,7 @@ const Prod=db.define('product',{
     primaryKey:true,
     autoIncrement: true,
   },
-  pnum:Sequelize.STRING,
+  pnum:Sequelize.INTEGER,
   des:Sequelize.TEXT,ades:Sequelize.TEXT,
   img2:Sequelize.STRING,img3:Sequelize.STRING,img4:Sequelize.STRING,
   cod:Sequelize.BOOLEAN,retrn:Sequelize.BOOLEAN,delivery:Sequelize.BOOLEAN,
